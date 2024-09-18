7b4670df85c0e8bea41560b9519c88cd99b9fda0
Saikrishna9929 <maddikuntasaikrishna77@gmail.com>
2024-09-18 15:38:22 +0530

    Updates

---
+++ precache-manifest.8bd38424d061b48a93f203aba5681650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696635dfdc4fb020f5527d9ab101753a",
+    "url": "/trafficLightSimulator/index.html"
+  },
+  {
+    "revision": "3196d147b8eca44a622a",
+    "url": "/trafficLightSimulator/static/css/main.4c01e11b.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/trafficLightSimulator/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/trafficLightSimulator/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "3196d147b8eca44a622a",
+    "url": "/trafficLightSimulator/static/js/main.b7b25cdc.chunk.js"
+  },
+  {
+    "revision": "4946d9fd530c084f1f10",
+    "url": "/trafficLightSimulator/static/js/runtime~main.17315206.js"
+  }
+]);
